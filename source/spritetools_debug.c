cac2bfd4ff55cd7d53693a52e97e30b073b331ca
Bryce BtheDestroyer Dixon <pokemonbrycedixon@gmail.com>
2016-12-28 22:23:32 -0800

    Patched up debugger so now all ctrulib variables work correctly

--- source/spritetools_debug.c
+++ source/spritetools_debug.c
@@ -8,6 +8,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <inttypes.h>
 #include <spritetools_debug.h>
 #include <spritetools_input.h>
 #include <spritetools_textcolors.h>
@@ -211,70 +212,70 @@ void *ST_DebugPrintVarId(int id)
     break;
 
     case U8 :
-    sprintf(tempstr, "%.2d. u8     %s = %u", id, DEBUGVars[id].name,
+    sprintf(tempstr, "%.2d. u8     %s = %" PRIu8, id, DEBUGVars[id].name,
       *(u8 *)DEBUGVars[id].pointer);
     break;
     case U16 :
-    sprintf(tempstr, "%.2d. u16    %s = %u", id, DEBUGVars[id].name,
+    sprintf(tempstr, "%.2d. u16    %s = %" PRIu16, id, DEBUGVars[id].name,
       *(u16 *)DEBUGVars[id].pointer);
     break;
     case U32 :
-    sprintf(tempstr, "%.2d. u32    %s = %lu", id, DEBUGVars[id].name,
+    sprintf(tempstr, "%.2d. u32    %s = %" PRIu32, id, DEBUGVars[id].name,
       *(u32 *)DEBUGVars[id].pointer);
     break;
     case U64 :
-    sprintf(tempstr, "%.2d. u64    %s = %llu", id, DEBUGVars[id].name,
+    sprintf(tempstr, "%.2d. u64    %s = %" PRIu64, id, DEBUGVars[id].name,
       *(u64 *)DEBUGVars[id].pointer);
     break;
 
     case S8 :
-    sprintf(tempstr, "%.2d. s8     %s = %d", id, DEBUGVars[id].name,
+    sprintf(tempstr, "%.2d. s8     %s = %" PRId8, id, DEBUGVars[id].name,
       *(s8 *)DEBUGVars[id].pointer);
     break;
     case S16 :
-    sprintf(tempstr, "%.2d. s16    %s = %d", id, DEBUGVars[id].name,
+    sprintf(tempstr, "%.2d. s16    %s = %" PRId16, id, DEBUGVars[id].name,
       *(s16 *)DEBUGVars[id].pointer);
     break;
     case S32 :
-    sprintf(tempstr, "%.2d. s32    %s = %ld", id, DEBUGVars[id].name,
+    sprintf(tempstr, "%.2d. s32    %s = %" PRId32, id, DEBUGVars[id].name,
       *(s32 *)DEBUGVars[id].pointer);
     break;
     case S64 :
-    sprintf(tempstr, "%.2d. s64    %s = %lld", id, DEBUGVars[id].name,
+    sprintf(tempstr, "%.2d. s64    %s = %" PRId64, id, DEBUGVars[id].name,
       *(s64 *)DEBUGVars[id].pointer);
     break;
 
     case VU8 :
-    sprintf(tempstr, "%.2d. vu8    %s = %u", id, DEBUGVars[id].name,
+    sprintf(tempstr, "%.2d. vu8    %s = %" PRIu8, id, DEBUGVars[id].name,
       *(vu8 *)DEBUGVars[id].pointer);
     break;
     case VU16 :
-    sprintf(tempstr, "%.2d. vu16   %s = %u", id, DEBUGVars[id].name,
+    sprintf(tempstr, "%.2d. vu16   %s = %" PRIu16, id, DEBUGVars[id].name,
       *(vu16 *)DEBUGVars[id].pointer);
     break;
     case VU32 :
-    sprintf(tempstr, "%.2d. vu32   %s = %lu", id, DEBUGVars[id].name,
+    sprintf(tempstr, "%.2d. vu32   %s = %" PRIu32, id, DEBUGVars[id].name,
       *(vu32 *)DEBUGVars[id].pointer);
     break;
     case VU64 :
-    sprintf(tempstr, "%.2d. vu64   %s = %llu", id, DEBUGVars[id].name,
+    sprintf(tempstr, "%.2d. vu64   %s = %" PRIu64, id, DEBUGVars[id].name,
       *(vu64 *)DEBUGVars[id].pointer);
     break;
 
     case VS8 :
-    sprintf(tempstr, "%.2d. vs8    %s = %d", id, DEBUGVars[id].name,
+    sprintf(tempstr, "%.2d. vs8    %s = %" PRIu8, id, DEBUGVars[id].name,
       *(vs8 *)DEBUGVars[id].pointer);
     break;
     case VS16 :
-    sprintf(tempstr, "%.2d. vs16   %s = %d", id, DEBUGVars[id].name,
+    sprintf(tempstr, "%.2d. vs16   %s = %" PRIu16, id, DEBUGVars[id].name,
       *(vs16 *)DEBUGVars[id].pointer);
     break;
     case VS32 :
-    sprintf(tempstr, "%.2d. vs32   %s = %ld", id, DEBUGVars[id].name,
+    sprintf(tempstr, "%.2d. vs32   %s = %" PRIu32, id, DEBUGVars[id].name,
       *(vs32 *)DEBUGVars[id].pointer);
     break;
     case VS64 :
-    sprintf(tempstr, "%.2d. vs64  %s = %lld", id, DEBUGVars[id].name,
+    sprintf(tempstr, "%.2d. vs64   %s = %" PRIu64, id, DEBUGVars[id].name,
       *(vs64 *)DEBUGVars[id].pointer);
     break;
 
