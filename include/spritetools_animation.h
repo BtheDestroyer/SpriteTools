3131180c30e8d1a5bcf2812bbb2064dafa300625
Bryce BtheDestroyer Dixon <pokemonbrycedixon@gmail.com>
2016-12-29 21:32:52 -0800

    Added stuff for creating animations. Recommended that you do not use this as it's likely broken/going to change

--- include/spritetools_animation.h
+++ include/spritetools_animation.h
@@ -22,6 +22,14 @@ typedef struct {
   unsigned int height;
 } st_frame;
 
+/* Animation of frames */
+typedef struct {
+  int fpf; /* Number of frames to wait between each frame of animation */
+  unsigned int loopFrame; /* Frame to jump to when an animation loops */
+  unsigned int length; /* Number of frames in the animation */
+  st_frame **frames;
+} st_animation;
+
 /***************************\
 |*     Frame Functions     *|
 \***************************/
@@ -30,12 +38,24 @@ typedef struct {
 /* Takes a pointer to a spritesheet */
 /*   Takes the position of the frame in the spritesheet */
 /*   Takes the dimensions of the frame */
-st_frame *ST_FrameCreateFrame(st_spritesheet *spritesheet,
+st_frame *ST_AnimationCreateFrame(st_spritesheet *spritesheet,
   unsigned int xleft, unsigned int ytop,
   unsigned int width, unsigned int height);
 
 /* Frees frame from memory */
 /* Takes a pointer to a frame */
-void ST_FrameFreeFrame(st_frame *frame);
+void ST_AnimationFreeFrame(st_frame *frame);
+
+/*******************************\
+|*     Animation Functions     *|
+\*******************************/
+/* Returns a pointer to an animation */
+/*   Returns NULL if failed */
+/* Takes speed of animation (in frames between each frame of animation) */
+/*   Takes frame to loop to when the animation has reached its end */
+/*   Takes length of animation in frames */
+/*   Takes a list of pointers to frames */
+st_animation *ST_AnimationCreateAnimation(int fpf, unsigned int loopFrame,
+  unsigned int length, ...);
 
 #endif
--- source/spritetools_animation.c
+++ source/spritetools_animation.c
@@ -5,6 +5,7 @@
 */
 
 #include <stdlib.h>
+#include <stdarg.h>
 #include <spritetools_animation.h>
 
 /***************************\
@@ -36,4 +37,38 @@ st_frame *ST_FrameCreateFrame(st_spritesheet *spritesheet,
 void ST_FrameFreeFrame(st_frame *frame)
 {
   free(frame);
+}
+
+/*******************************\
+|*     Animation Functions     *|
+\*******************************/
+/* Returns a pointer to an animation */
+/*   Returns NULL if failed */
+/* Takes speed of animation (in frames between each frame of animation) */
+/*   Takes frame to loop to when the animation has reached its end */
+/*   Takes length of animation in frames */
+/*   Takes a list of pointers to frames */
+st_animation *ST_AnimationCreateAnimation(int fpf, unsigned int loopFrame,
+  unsigned int length, ...)
+{
+  int i;
+  va_list ap;
+
+  st_animation *tempanim = calloc(sizeof(st_animation), 1);
+  if (!tempanim)
+    return 0;
+
+  tempanim->fpf = fpf;
+  tempanim->loopFrame = loopFrame;
+  tempanim->length = length;
+  tempanim->frames = calloc(sizeof(st_frame*), length);
+
+  va_start(ap, length);
+
+  for (i = 0; i < length; i++)
+    tempanim->frames[i] = va_arg(ap, st_frame*);
+
+  va_end (ap);
+
+  return tempanim;
 }
