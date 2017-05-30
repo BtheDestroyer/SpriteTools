55840c36c2bfc59b03327475ac2e6713255fb323
Bryce "BtheDestroyer" Dixon <pokemonbrycedixon@gmail.com>
2017-05-29 23:49:15 -0700

    Collision checking between points and all types and two circles

--- include/spritetools/spritetools_collision.h
+++ include/spritetools/spritetools_collision.h
@@ -95,9 +95,6 @@ u8 ST_CollisionCheckPointPoint(st_hitboxpoint *hb1, st_hitboxpoint *hb2);
 /* Point and circle */
 u8 ST_CollisionCheckPointCircle(st_hitboxpoint *hb1, st_hitboxcircle *hb2);
 
-/* Point and line */
-u8 ST_CollisionCheckPointLine(st_hitboxpoint *hb1, st_hitboxline *hb2);
-
 /* Point and rect */
 u8 ST_CollisionCheckPointRect(st_hitboxpoint *hb1, st_hitboxrect *hb2);
 /******************************************************************************/
@@ -107,32 +104,14 @@ u8 ST_CollisionCheckCirclePoint(st_hitboxcircle *hb1, st_hitboxpoint *hb2);
 /* Circle and circle */
 u8 ST_CollisionCheckCircleCircle(st_hitboxcircle *hb1, st_hitboxcircle *hb2);
 
-/* Circle and line */
-u8 ST_CollisionCheckCircleLine(st_hitboxcircle *hb1, st_hitboxline *hb2);
-
 /* Circle and rect */
 u8 ST_CollisionCheckCircleRect(st_hitboxcircle *hb1, st_hitboxrect *hb2);
-/******************************************************************************/
-/* Line and point */
-u8 ST_CollisionCheckLinePoint(st_hitboxline *hb1, st_hitboxpoint *hb2);
-
-/* Line and circle */
-u8 ST_CollisionCheckLineCircle(st_hitboxline *hb1, st_hitboxcircle *hb2);
-
-/* Line and line */
-u8 ST_CollisionCheckLineLine(st_hitboxline *hb1, st_hitboxline *hb2);
-
-/* Line and rect */
-u8 ST_CollisionCheckLineRect(st_hitboxline *hb1, st_hitboxrect *hb2);
 /******************************************************************************/
 /* Rect and point */
 u8 ST_CollisionCheckRectPoint(st_hitboxrect *hb1, st_hitboxpoint *hb2);
 
 /* Rect and circle */
 u8 ST_CollisionCheckRectCircle(st_hitboxrect *hb1, st_hitboxcircle *hb2);
-
-/* Rect and line */
-u8 ST_CollisionCheckRectLine(st_hitboxrect *hb1, st_hitboxline *hb2);
 
 /* Rect and rect */
 u8 ST_CollisionCheckRectRect(st_hitboxrect *hb1, st_hitboxrect *hb2);
--- source/spritetools_collision.c
+++ source/spritetools_collision.c
@@ -135,6 +135,7 @@ void ST_HitboxFreeRect(st_hitboxrect *hitbox)
 /******************************\
 |*     Collision Checking     *|
 \******************************/
+/******************************************************************************/
 /* Point and point */
 u8 ST_CollisionCheckPointPoint(st_hitboxpoint *hb1, st_hitboxpoint *hb2)
 {
@@ -144,46 +145,53 @@ u8 ST_CollisionCheckPointPoint(st_hitboxpoint *hb1, st_hitboxpoint *hb2)
 }
 
 /* Point and circle */
-u8 ST_CollisionCheckPointCircle(st_hitboxpoint *hb1, st_hitboxcircle *hb2);
-
-/* Point and line */
-u8 ST_CollisionCheckPointLine(st_hitboxpoint *hb1, st_hitboxline *hb2);
+u8 ST_CollisionCheckPointCircle(st_hitboxpoint *hb1, st_hitboxcircle *hb2)
+{
+  double xdiff = hb1->xpos - hb2->xpos;
+  double ydiff = hb1->ypos - hb2->ypos;
+  if (hb2->radius * hb2->radius <= xdiff * xdiff + ydiff * ydiff)
+    return 1;
+  return 0;
+}
 
 /* Point and rect */
-u8 ST_CollisionCheckPointRect(st_hitboxpoint *hb1, st_hitboxrect *hb2);
+u8 ST_CollisionCheckPointRect(st_hitboxpoint *hb1, st_hitboxrect *hb2)
+{
+  if (hb1->xpos >= hb2->xpos && hb1->xpos <= hb2->xpos + hb2->width &&
+      hb1->ypos >= hb2->ypos && hb1->ypos <= hb2->ypos + hb2->height)
+    return 1;
+  return 0;
+}
 /******************************************************************************/
 /* Circle and point */
-u8 ST_CollisionCheckCirclePoint(st_hitboxcircle *hb1, st_hitboxpoint *hb2);
+u8 ST_CollisionCheckCirclePoint(st_hitboxcircle *hb1, st_hitboxpoint *hb2)
+{
+  double xdiff = hb1->xpos - hb2->xpos;
+  double ydiff = hb1->ypos - hb2->ypos;
+  if (hb1->radius * hb1->radius <= xdiff * xdiff + ydiff * ydiff)
+    return 1;
+  return 0;
+}
 
 /* Circle and circle */
-u8 ST_CollisionCheckCircleCircle(st_hitboxcircle *hb1, st_hitboxcircle *hb2);
-
-/* Circle and line */
-u8 ST_CollisionCheckCircleLine(st_hitboxcircle *hb1, st_hitboxline *hb2);
+u8 ST_CollisionCheckCircleCircle(st_hitboxcircle *hb1, st_hitboxcircle *hb2)
+{
+  double xdiff = hb1->xpos - hb2->xpos;
+  double ydiff = hb1->ypos - hb2->ypos;
+  double radius = hb1->radius + hb2->radius;
+  if (radius * radius  <= xdiff * xdiff + ydiff * ydiff)
+    return 1;
+  return 0;
+}
 
 /* Circle and rect */
 u8 ST_CollisionCheckCircleRect(st_hitboxcircle *hb1, st_hitboxrect *hb2);
-/******************************************************************************/
-/* Line and point */
-u8 ST_CollisionCheckLinePoint(st_hitboxline *hb1, st_hitboxpoint *hb2);
-
-/* Line and circle */
-u8 ST_CollisionCheckLineCircle(st_hitboxline *hb1, st_hitboxcircle *hb2);
-
-/* Line and line */
-u8 ST_CollisionCheckLineLine(st_hitboxline *hb1, st_hitboxline *hb2);
-
-/* Line and rect */
-u8 ST_CollisionCheckLineRect(st_hitboxline *hb1, st_hitboxrect *hb2);
 /******************************************************************************/
 /* Rect and point */
 u8 ST_CollisionCheckRectPoint(st_hitboxrect *hb1, st_hitboxpoint *hb2);
 
 /* Rect and circle */
 u8 ST_CollisionCheckRectCircle(st_hitboxrect *hb1, st_hitboxcircle *hb2);
-
-/* Rect and line */
-u8 ST_CollisionCheckRectLine(st_hitboxrect *hb1, st_hitboxline *hb2);
 
 /* Rect and rect */
 u8 ST_CollisionCheckRectRect(st_hitboxrect *hb1, st_hitboxrect *hb2);
